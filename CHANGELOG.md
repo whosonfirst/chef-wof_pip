e86e1be5cfc19c73e0c36d8231a3966aa046050d
Grant Heffernan <heffergm@gmail.com>
2016-01-27 18:54:34 -0500

    - 0.3.0 - `-cache_all` as default, overrides `-cache_size` and `-cache_trigger`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 CHANGELOG
 =========
 
+0.3.0
+-----
+* add `-cache_all` as default, overrides `-cache_size` and `-cache_trigger`
+
 0.2.1
 -----
 * purge before data run if true
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures wof_pip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.3.0'
 
 %w(
   apt
