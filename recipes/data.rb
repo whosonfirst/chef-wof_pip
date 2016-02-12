927522da9ebc18ef1984563f3845abb90bbd37bc
Grant Heffernan <heffergm@gmail.com>
2016-02-11 20:21:16 -0500

    bug fix for data updates that include new regions not updating the service

--- recipes/data.rb
+++ recipes/data.rb
@@ -7,12 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'wof_pip::_setup'
+include_recipe 'wof_pip::service'
 include_recipe 'wof_pip::_data_bundles' if node[:wof_pip][:data][:source] == :bundles
 include_recipe 'wof_pip::_data_files'   if node[:wof_pip][:data][:source] == :files
 
 file node[:wof_pip][:data][:initial_run_complete_file]
-
-runit_service 'wof-pip-server' do
-  action :nothing
-end
