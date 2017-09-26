80f64522be0a4db28a1295029015453e82652cbd
thisisaaroland <aaron@mapzen.com>
2017-09-26 12:06:03 -0700

    update to use go-whosonfirst-pip-v2

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,11 +12,11 @@ default[:wof_pip][:log][:dir]           = '/wof/logs'
 
 # wof-pip-server
 #
-default[:wof_pip][:server][:repository]     = 'https://github.com/whosonfirst/go-whosonfirst-pip.git'
-default[:wof_pip][:server][:revision]       = 'geojson-v2'
+default[:wof_pip][:server][:repository]     = 'https://github.com/whosonfirst/go-whosonfirst-pip-v2.git'
+default[:wof_pip][:server][:revision]       = 'master'
 default[:wof_pip][:server][:port]           = 9998
 default[:wof_pip][:server][:cache_all]      = true    # if true, ignores cache_size/cache_trigger
-default[:wof_pip][:server][:cache_size]     = 10_000  # number of records to cache
+default[:wof_pip][:server][:cache_size]     = 10000  # number of records to cache
 default[:wof_pip][:server][:cache_trigger]  = 2000    # number of file record to trigger caching
 default[:wof_pip][:server][:bind]           = 'localhost'
 default[:wof_pip][:server][:loglevel]       = 'info'
